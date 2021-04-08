63ecc628304e7506873a3145b2e52ab00da30c83
PriyavardhanRathore <priyavarhanr99@gmail.com>
2021-04-08 14:55:25 +0530

    added new assignment 7/04/2021

---
+++ Assignment3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                int row, column;
+
+                for (row = 0; row <= 6; row++)
+                {
+                    for (column = 0; column <= 6; column++)
+                    {
+                        if (column == 1 || ((row == 0 || row == 3) && column > 1 && column < 5) || (column == 5 && row != 0 && row < 3) || (column == row - 1 && row > 2))
+                            Console.Write("*");
+                        else
+                            Console.Write(" ");
+                    }
+                    Console.Write("\n");
+                }
+                Console.Write("\n");
+            Console.ReadLine();
+        }
+    }
+}
